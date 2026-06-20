6ef107811046fa43ca5e8a0da5569ecc0fe834a9
Private Developer <tareknegoudi@gmail.com>
2026-06-20 13:22:39 +0100

    Add path module import to server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const WebSocket = require('ws');
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
+const path = require('path');
 // تقديم الملفات الثابتة من مجلد public
 app.use(express.static('public'));
 
